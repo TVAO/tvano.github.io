438446ebcd12b22a456a8714b2cd137ce2cfff53
TVANO <thorolesen14@gmail.com>
2017-08-14 01:13:21 +0200

    Trying to fix bullet points in work section. Need to finish education, skills and portfolio and add styling

---
+++ src/components/bullet_points.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+const BulletPoints = ({props}) => (
+  <div></div>
+);
+
+export default BulletPoints;
+
+/*
+
+{props.map((point, index) => {
+    return (
+        <p key={index} className='point'>
+            <span className='bullet-point'> &bull; </span>
+            {point}
+        </p>
+    );
+})}
+
+ */
--- src/components/work.js
+++ src/components/work.js
@@ -1,39 +1,50 @@
 import React from 'react';
+import BulletPoints from '../components/bullet_points';
 
-const Entry = ({entry}) => (
-  <div className="row item">
-      <div className="twelve columns">
-        <h3>
-          <a href={entry.website}> {entry.company} </a>
-        </h3>
-        <p className="info">
-          {entry.position}
-          <span> &bull; </span>
-          <span className="info-summary">{entry.summary}</span>
-          <span> &bull; </span>
-          <em className="date"> {entry.startDate} { entry.endDate ? `- ${entry.endDate}` : "" } </em>
-        </p>
-      </div>
-  </div>
-);
+const Entry = ({key, entry, index, total}) => {
 
-const Work = ({props}) => (
-  <section id="work">
-    <div className="row work">
-      <div className="two columns header-col">
-        <h1>
-          <span> Work </span>
-        </h1>
-      </div>
-      <div className="ten columns main-col">
-        {props.map(function (entry, index) {
-          return (
-              <Entry key={index} entry={entry}/>
-          );
-        })}
-      </div>
+  const newIndex = index + 1;
+  const divider = newIndex === total ? (<br/>) : (<hr/>);
+
+  return (
+    <div className="row item">
+        <div className="twelve columns">
+          <h3>
+            <a href={entry.website}> {entry.company} </a>
+          </h3>
+          <p className="info">
+            {entry.position}
+            <span> &bull; </span>
+            <em className="date"> {entry.startDate} { entry.endDate ? `- ${entry.endDate}` : "" } </em>
+          </p>
+          <BulletPoints points={entry.highlights}/>
+        </div>
+        {divider}
     </div>
-  </section>
-);
+  );
+}
+
+const Work = ({props}) => {
+  const numEntries = props.length;
+
+  return (
+    <section id="work">
+      <div className="row work">
+        <div className="two columns header-col">
+          <h1>
+            <span> Work </span>
+          </h1>
+        </div>
+        <div className="ten columns main-col">
+          {props.map((entry, index) => {
+            return (
+                <Entry key={index} entry={entry} index={index} total={numEntries}/>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default Work;
